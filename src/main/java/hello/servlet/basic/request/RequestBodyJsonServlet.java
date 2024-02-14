8b6957c1ce11b5b8af62e40879f4965fcce05c80
jjang0308 <jang653838@naver.com>
2024-02-14 15:19:47 +0900

    project created

--- src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
@@ -1,5 +1,7 @@
 package hello.servlet.basic.request;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
 import org.springframework.util.StreamUtils;
 
 import javax.servlet.ServletException;
@@ -13,10 +15,20 @@ import java.nio.charset.StandardCharsets;
 
 @WebServlet(name = "requestBodyJsonServlet" , urlPatterns = "/request-body-json")
 public class RequestBodyJsonServlet extends HttpServlet {
+
+    //json 결과를 파싱해서 사용할 수 있는 자바객체로 변환하려면 jackson같은 json변환 라이브러리를 추가해서 사용.
+    private ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         ServletInputStream inputStream = request.getInputStream();
         String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
         System.out.println("messageBody = " + messageBody);
+
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+        System.out.println("helloData.userName = " + helloData.getUserName());
+        System.out.println("helloData.age = " + helloData.getAge());
+
+        response.getWriter().write("ok");
     }
 }
